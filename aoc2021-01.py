b06c8aecfe933d5dccb4fd31593379a8d70570a2
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-12-01 23:18:41 +0100

    Fixade ett fel, verkar som att jag hade tur.

--- aoc2021-01.py
+++ aoc2021-01.py
@@ -3,7 +3,7 @@ lines = [int(x) for x in fil.read().split('\n')[:-1]]
 
 def partone():
     print("Advent of Code 2021, day 1, part 1.")
-    ans = len([1 for i in range(len(lines)) if lines[i] > lines[i-1]])
+    ans = len([1 for i in range(1, len(lines)) if lines[i] > lines[i-1]])
     print("The answer is:", ans)
 
 def parttwo():
