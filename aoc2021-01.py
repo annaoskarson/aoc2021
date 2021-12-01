6a5f6bdfd1c4173198630ac4e271bf96c38cfb3f
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-12-01 17:07:21 +0100

    Fixat list comprehension.

--- aoc2021-01.py
+++ aoc2021-01.py
@@ -3,27 +3,13 @@ lines = [int(x) for x in fil.read().split('\n')[:-1]]
 
 def partone():
     print("Advent of Code 2021, day 1, part 1.")
-    i = 1
-    no = 0
-
-    while i < len(lines):
-        if lines[i] > lines[i-1]:
-            no += 1
-        i +=1
-    print("The answer is:", no)
+    ans = len([1 for i in range(len(lines)) if lines[i] > lines[i-1]])
+    print("The answer is:", ans)
 
 def parttwo():
     print("Advent of Code 2021, day 1, part 2.")
-    i = 3
-    no = 0
-
-    while i < len(lines):
-        A = lines[i-1:i-4]
-        B = lines[i:i-2]
-        if B > A:
-            no +=1
-        i += 1
-    print("The answer is:", no)
+    ans = len([1 for i in range(3, len(lines)) if sum(lines[i:i-3:-1]) > sum(lines[i-1:i-4:-1])])
+    print("The answer is:", ans)
 
 partone()
 parttwo()
