3228153d366bd1180d1f8b60d743c110c5e1dead
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-12-12 13:26:08 +0100

    Comments in English.

--- aoc2021-12.py
+++ aoc2021-12.py
@@ -1,56 +1,54 @@
-map = {}
 with open('12.txt', 'r') as fil:
     m = ([l.strip().split('-') for l in fil ])
 
+map = {} # Map is a dict with cave containing set of neigbhours.
 for [t, n] in m:
-    if t not in map:
-        map[t] = set([n])
-    else:
-        map[t].add(n)
-    if n not in map:
-        map[n] = set([t])
-    else:
-        map[n].add(t)
+    for (a,b) in [(t,n), (n,t)]: # Neighbours works both ways.
+        if a not in map:
+            map[a] = set([b])
+        else:
+            map[a].add(b)
 
 def partone(map):
     print("Advent of Code 2021, day 12, part 1.")
-    ways = []
     def wayout(map, way):
-        this = way[-1]
+        this = way[-1] # Where we are now.
         if this == 'end':
-            ways.append(way)
+            ways.append(way) # A way is completed.
         else:
-            nbs = map[this] # neighbours to visit
+            nbs = map[this] # Neighbours to probably visit.
             nbs = nbs - set([n for n in nbs if n.islower() and n in way])
-            # Man får inte gå tillbaka till start(?) eller till någon liten grotta man redan besökt.
+            # Never go back into a small cave.
             for n in nbs:
                 wayout(map, way + [n])
 
+    ways = []
     wayout(map, ['start'])
     print("The answer is:", len(ways))
 
 def parttwo(map):
     print("Advent of Code 2021, day 9, part 2.")
-    ways = []
     def wayout(map, way, twice):
-        # twice == True om den är utnyttjad redan
-        this = way[-1]
+        # twice is True if the twice rule is used.
+        this = way[-1] # Where we are now.
         if this == 'end':
-            ways.append(way)
+            ways.append(way) # A way is completed.
         else:
-            nbs = map[this] # neighbours to visit
-            nbs = nbs - set(['start'])
+            nbs = map[this] # neighbours to probably visit
+            nbs = nbs - set(['start']) # never go back to start
             if twice:
                 nbs = nbs - set([n for n in nbs if n.islower() and n in way])
-                # Ta bort små grottor om twice är utnyttjad.
+                # Remove small caves if twice is used.
             for n in nbs:
                 if n.islower() and n in way and not twice:
-                    #OK att besöka n, men sätt twice till True.
+                    # OK to visit, but then twice is used.
                     wayout(map, way + [n], True)
                 else:
+                    # Ordinary visit, pass twice through.
                     wayout(map, way + [n], twice)
 
-    wayout(map, ['start'], False)
+    ways = []
+    wayout(map, ['start'], False) # Start from 'start' without twice used.
     print("The answer is:", len(ways))
 
 partone(map)
