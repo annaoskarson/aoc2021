704ca4f21723e0b9170aed071253842f87a0d5bb
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-12-18 01:08:41 +0100

    Nu ännu bättre.

--- aoc2021-14.py
+++ aoc2021-14.py
@@ -2,71 +2,47 @@ with open('14.txt', 'r') as fil:
     lines = fil.read().strip().split('\n')
 
 p = lines[0]
+#print(p)
 subs = {}
 for l in lines[2:]:
     a, b = l.split(' -> ')
     subs[a] = b
 
-def subb(st, su):
-    newst = []
-    st = list(st)
-    for i, c in enumerate(st[:-1]):
-        pair = st[i:i+2]
-        if ''.join(pair) in su:
-            new = st[i] + su[''.join(pair)]
-            newst.append(new)
-        else:
-            newst += c
-    newst += st[-1]
-    return(''.join(newst))
+def generate(old, su):
+    newcontent = {}
+    for pair in old:
+        newpairs = [pair[0] + su[pair], su[pair] + pair[1]]
+        for p in newpairs:
+            if p not in newcontent:
+                newcontent[p] = 0
+            newcontent[p] += old[pair]
+    return(newcontent)
 
-import collections
-def ans(p):
-    times = [b for a,b in list(cnt(p))]
-    return(max(times) - min(times))
+def part(p, subs, times):
+    content = {}
+    for i in range(len(p)-1):
+        pair = p[i:i+2]
+        if pair not in content:
+            content[pair] = 0
+        content[pair] += 1
+    for i in range(times):
+        content = generate(content, subs)
+    results = {}
+    for pair in content:
+        c = pair[0]
+        if c not in results:
+            results[c] = 0
+        results[c] += content[pair]
+    c = p[-1]
+    if c not in results:
+        results[c] = 0
+    results[c] += 1
+    ans = max(results.values())- min(results.values())
+    return(ans)
 
-def cnt(p):
-    return(collections.Counter(list(p)).most_common())
-
-def partone(p, subs):
-    print('Advent of Code 2021, day 14 part 1.')
-    for i in range(10):
-        p = subb(p, subs)
-    print('The answer is', ans(p))
-
-partone(p, subs)
-
-def translation(p, subs, times):
-    if times == 20:
-        return(ans(p[:-1]), cnt(p[:-1]))
-    else:
-        p = subb(p, subs)
-        times += 1
-        return(translation(p, subs, times))
-
-twenties = {}
-for pair in subs.keys():
-    twenties[pair] = translation(pair, subs, 0)
-
-def parttwo(p, subs):
-    print('Advent of Code 2021, day 14 part 2.')
-    for i in range(20):
-        print(i)
-        p = subb(p, subs)
-    p = list(p)
-    print(len(p))
-
-    fourties = {}
-    for c in set(p):
-        fourties[c]= 0
-    for i in range(len(p)):
-        if i > 0:
-            pair = ''.join(p[i-1:i+1])
-            apa = twenties[pair][1]
-            for c, nb in twenties[pair][1]:
-                fourties[c] += nb
-    fourties[p[-1]] += 1
-    diff = max(fourties.values()) - min(fourties.values())
-    print('The answer is', diff)
-
-parttwo(p, subs)
+print('Advent of Code 2021, day 14 part 1.')
+ans = part(p, subs, 10)
+print('The answer is:', ans)
+print('Advent of Code 2021, day 14 part 2.')
+ans = part(p, subs, 40)
+print('The answer is:', ans)
