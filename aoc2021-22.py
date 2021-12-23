1ae63c53c57b7f6190e422be126b1b9c2eb01c0a
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-12-23 12:44:28 +0100

    Dag 22, del 2 klar!

--- aoc2021-22.py
+++ aoc2021-22.py
@@ -1,16 +1,13 @@
-with open('22-t1.txt', 'r') as fil:
+with open('22.txt', 'r') as fil:
     lines = fil.read().strip().split('\n')
 
-#print(lines)
-
-coords = set()
+import copy
 
 def partone(lines):
     print('Advent of Code, day 22, part 1.')
     for l in lines:
         com = l.split(' ')[0]
         xmin, xmax, ymin, ymax, zmin, zmax = [int(b) for a in l.split(' ')[1].strip().split(',') for b in a.strip().split('=')[1].split('..')]
-        #print(com, (xmin, xmax, ymin, ymax, zmin, zmax))
 
         if xmin > -50 and xmax < 50 and ymin > -50 and ymax < 50 and zmin > -50 and zmax < 50:
 
@@ -32,178 +29,148 @@ def partone(lines):
     print('The answer is', len(coords))
 
 #partone(lines)
+test = False
+def outside(c2, c1):
+    xout = (c2['x'][0] > c1['x'][1]) or (c2['x'][1] < c1['x'][0])
+    yout = (c2['y'][0] > c1['y'][1]) or (c2['y'][1] < c1['y'][0])
+    zout = (c2['z'][0] > c1['z'][1]) or (c2['z'][1] < c1['z'][0])
+    out = any([xout, yout, zout])
+    return(out)
 
-def parttwo(lines):
+def inside(outer, inner):
+    xin = (outer['x'][0] <= inner['x'][0]) and (inner['x'][1] <= outer['x'][1])
+    yin = (outer['y'][0] <= inner['y'][0]) and (inner['y'][1] <= outer['y'][1])
+    zin = (outer['z'][0] <= inner['z'][0]) and (inner['z'][1] <= outer['z'][1])
+    allin = all([xin, yin, zin])
+    return(allin)
 
-    def outside(c2, c1):
-        #print('apa', c2, c1)
-        #print(list((a2[1] < a1[0]) or (a2[0] > a1[1]) for a2 in c2.values() for a1 in c1.values()))
-        xout = (c2['x'][0] > c1['x'][1]) or (c2['x'][1] < c1['x'][0])
-        yout = (c2['y'][0] > c1['y'][1]) or (c2['y'][1] < c1['y'][0])
-        zout = (c2['z'][0] > c1['z'][1]) or (c2['z'][1] < c1['z'][0])
-        out = any([xout, yout, zout])
-        #out = any((a2[1] < a1[0]) or (a2[0] > a1[1]) for a2 in c2.values() for a1 in c1.values())
-        return(out)
+def addcuboid(nspace, cuboid):
+    if len(nspace) == 0:
+        # If somehow space is empty.
+        return([cuboid])
 
-    def addcuboid(space, cuboid):
-        print('adding', len(space), cuboid)
-        if len(space) == 0:
-            # If somehow space is empty.
-            print('empty space, returning only the cuboid added.')
-            return([cuboid])
+    # First, make place in space for the new cuboid.
+    nspace = remcuboid(nspace, cuboid)
 
-        print('before rem in add', spaceon(space), space)
-        space = remcuboid(space, cuboid)
-        print('after rem in add', spaceon(space), space)
+    # Now space is clean from the cuboid.
+    # Add the cuboid to space
+    nspace.append(cuboid)
+    return(nspace)
 
-        # Now space is clean from the cuboid.
-        # Add the cuboid to space
-        space.append(cuboid)
-        print('after adding in add', spaceon(space))
+def remcuboid(nspace, cuboid):
+    if len(nspace) == 0:
+        return(nspace)
 
-        return(space)
+    newcuboids = []
+    for part in nspace:
 
-    def remcuboid(space, cuboid):
+        if outside(cuboid, part):
+             # Add the part untouched.
+             newcuboids.append(part)
 
-        if len(space) == 0:
-            print('space is empty')
-            return(space)
+        elif inside(cuboid, part):
+            pass
 
-        newcuboids = []
-        for i, part in enumerate(space):
-
-            if outside(cuboid, part):
-                print('outside!')
-                break
+        else:
+            # Booleans for defining walls easier.
+            roof, floor, left, right = False, False, False, False
 
             if part['y'][1] > cuboid['y'][1]:
                 # There is a roof on the house.
+                roof = True
                 roof = {}
                 roof['x'] = part['x']
                 roof['z'] = part['z']
-                roof['y'] = (cuboid['y'][1], part['y'][1])
-                print('roof', roof)
+                roof['y'] = (cuboid['y'][1]+1, part['y'][1])
                 newcuboids.append(roof)
 
             if part['y'][0] < cuboid['y'][0]:
                 # There is a floor on the house.
+                floor = True
                 floor = {}
                 floor['x'] = part['x']
                 floor['z'] = part['z']
-                floor['y'] = (part['y'][0], cuboid['y'][0])
-                print('floor', floor)
+                floor['y'] = (part['y'][0], cuboid['y'][0]-1)
                 newcuboids.append(floor)
 
             if part['x'][0] < cuboid['x'][0]:
                 # There is a wall to the left.
+                left = True
                 wall = {}
-                wall['y'] = cuboid['y']
+                wall['y'] = part['y']
+                if floor: wall['y'] = (cuboid['y'][0], wall['y'][1])
+                if roof: wall['y'] = (wall['y'][0], cuboid['y'][1])
                 wall['z'] = part['z']
-                wall['x'] = (part['x'][0], cuboid['x'][0])
-                print('leftwall', wall)
+                wall['x'] = (part['x'][0], cuboid['x'][0]-1)
                 newcuboids.append(wall)
 
             if part['x'][1] > cuboid['x'][1]:
                 # There is a wall to the right.
+                right = True
                 wall = {}
-                wall['y'] = cuboid['y']
+                wall['y'] = part['y']
+                if floor: wall['y'] = (cuboid['y'][0], wall['y'][1])
+                if roof: wall['y'] = (wall['y'][0], cuboid['y'][1])
                 wall['z'] = part['z']
-                wall['x'] = (part['x'][0], cuboid['x'][0])
-                print('rightwall', wall)
+                wall['x'] = (cuboid['x'][1]+1, part['x'][1])
                 newcuboids.append(wall)
 
             if part['z'][0] < cuboid['z'][0]:
                 # There is a wall near.
                 wall = {}
-                wall['x'] = cuboid['x']
-                wall['y'] = cuboid['y']
-                wall['z'] = (part['z'][0], cuboid['z'][0])
-                print('nearwall', wall)
+                wall['y'] = part['y']
+                if floor: wall['y'] = (cuboid['y'][0], wall['y'][1])
+                if roof: wall['y'] = (wall['y'][0], cuboid['y'][1])
+                wall['x'] = part['x']
+                if left: wall['x'] = (cuboid['x'][0], wall['x'][1])
+                if right: wall['x'] = (wall['x'][0], cuboid['x'][1])
+                wall['z'] = (part['z'][0], cuboid['z'][0]-1)
                 newcuboids.append(wall)
 
             if part['z'][1] > cuboid['z'][1]:
                 # There is a wall in the distance.
                 wall = {}
-                wall['x'] = cuboid['x']
-                wall['y'] = cuboid['y']
-                wall['z'] = (cuboid['z'][1], part['z'][1])
-                print('farwall', wall)
+                wall['y'] = part['y']
+                if floor: wall['y'] = (cuboid['y'][0], wall['y'][1])
+                if roof: wall['y'] = (wall['y'][0], cuboid['y'][1])
+                wall['x'] = part['x']
+                if left : wall['x'] = (cuboid['x'][0], wall['x'][1])
+                if right: wall['x'] = (wall['x'][0], cuboid['x'][1])
+                wall['z'] = (cuboid['z'][1]+1, part['z'][1])
                 newcuboids.append(wall)
 
-            print('newcuboids', len(newcuboids))
+    return(newcuboids)
 
-        return(newcuboids)
+def spaceon(space):
+    spacesize = 0
+    nspace = copy.deepcopy(space)
+    i = 0
+    while len(nspace) > 0:
+        part = nspace.pop(0)
+        x = part['x'][1]-part['x'][0] +1
+        y = part['y'][1]-part['y'][0] +1
+        z = part['z'][1]-part['z'][0] +1
+        size = x*y*z
+        spacesize += size
+    return(spacesize)
 
-    def spaceon(space):
-        spacesize = 0
-        for part in space:
-            x = part['x'][1]-part['x'][0]
-            y = part['y'][1]-part['y'][0]
-            z = part['z'][1]-part['z'][0]
-            size = x*y*z
-            spacesize += size
-        return(spacesize)
-
+def parttwo(lines):
     print('Advent of Code, day 22, part 2.')
-    space = []
 
-    #a = {'x': (0, 10), 'y': (0, 10), 'z': (0, 10)}
-    #b = {'x': (2, 10), 'y': (0, 10), 'z': (0, 10)}
-    #c = {'x': (-2, 10), 'y': (0, 10), 'z': (0, 10)}
+    space = [] # This is where all light cuboids are stored.
 
-    # print()
-    # #print(spaceon([a]))
-    # space = addcuboid([a], b)
-    # print(spaceon(space))
-    # print(len(space), space)
-    # print()
-    #
-    # space = addcuboid([a], c)
-    # print(spaceon(space))
-    # print(len(space), space)
-    # print()
-    #
-    # print(spaceon([a]))
-    # space = remcuboid([a], b)
-    # print(spaceon(space))
-    # print(len(space), space)
-    # space = addcuboid(space, c)
-    # print(spaceon(space))
-    # print(len(space), space)
-    #
-    # exit()
-
-    for l in lines:
+    for i,l in enumerate(lines):
         com = l.split(' ')[0]
         xmin, xmax, ymin, ymax, zmin, zmax = [int(b) for a in l.split(' ')[1].strip().split(',') for b in a.strip().split('=')[1].split('..')]
         cuboid = {'x': (xmin, xmax), 'y': (ymin, ymax), 'z': (zmin, zmax)}
-        #print(cuboid)
-        if com == 'on':
-            print('on', cuboid)
+
+        if com == 'on': # Add the light cuboid to space.
             space = addcuboid(space, cuboid)
-        elif com == 'off':
-            print('off', cuboid)
+        elif com == 'off': # Remove the dark cuboid from space.
             space = remcuboid(space, cuboid)
-        print('space', len(space))
-        print()
-        #if l == lines[5]:
-        #    exit()
-    #print(len(space))
+
     ans = spaceon(space)
 
     print('The answer is', ans)
 
-#    testcuboid = {'x': (0, 10), 'y': (11, 21), 'z': (12, 22)}
-#    print(spaceon([testcuboid]))
-#    corners = []
-#    for z in testcuboid['z']:
-#        for y in testcuboid['y']:
-#            for x in testcuboid['x']:
-#                corners.append((x,y,z))
-#    print(corners)
-#    testcuboid = {'x': (0, 10), 'y': (1, 11), 'z': (2, 12)}
-#    print(spaceon([testcuboid]))
-
 parttwo(lines)
-
-# 11012440225185 too low
