6a0484e7d8bc52c07c84760b8d3a68ef9d012571
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-12-28 22:25:19 +0100

    Dag 21, del 2, haha!

--- aoc2021-21.py
+++ aoc2021-21.py
@@ -13,7 +13,7 @@ def roll(last):
     return(sum(steps), last)
 
 def partone(state):
-    print('Advent of Code, day 21, part 1.')
+    print('Advent of Code, day 21 part 1.')
     points = {1:0, 2:0}
     i = 0
     die = 1
@@ -23,9 +23,51 @@ def partone(state):
         (steps, die) = roll(die)
         state[player] = walk(state[player], steps)
         points[player] += state[player]
-        #print('turn:', player, 'steg', steps, 'landar på', state[player], 'total poäng', points[player])
         i += 3
     ans = (min(points.values()) * i)
     print('The answer is:', ans)
 
+def playgame(pturn, scores, positions):
+    global book
+
+    if (pturn, scores, positions) in book.keys():
+        return(book[(pturn,scores, positions)])
+    result = (0, 0)
+    for d1 in [1, 2, 3]:
+        for d2 in [1, 2, 3]:
+            for d3 in [1, 2, 3]:
+                steps = sum([d1, d2, d3])
+                thispos = walk(positions[pturn], steps)
+                thisscore = scores[pturn] + thispos
+
+                if thisscore >= 21: # win
+                    if pturn == 0:
+                        result = (result[0] + 1, result[1])
+                    else:
+                        result = (result[0], result[1] + 1)
+                else:
+                    newturn = 1 - pturn
+                    if pturn == 0:
+                        newscore = (thisscore, scores[1])
+                        newpos = (thispos, positions[1])
+                    else:
+                        newscore = (scores[0], thisscore)
+                        newpos = (positions[0], thispos)
+
+                    newresult = playgame(newturn, newscore, newpos)
+                    result = (result[0] + newresult[0], result[1] + newresult[1])
+
+    book[(pturn, scores, positions)] = result
+    return(result)
+
+def parttwo(state):
+    global cnt
+    print('Advent of Code, day 21 part 2.')
+    positions = (state[1], state[2])
+    results = playgame(0, (0, 0), positions)
+    ans = max(results)
+    print('The answer is:', ans)
+
 partone(state)
+book = {}
+parttwo(state)
